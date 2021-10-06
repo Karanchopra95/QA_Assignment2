324dd02a09a85edac47030aae8bb5ab39fc40be5
KaranChopra95 <karanchopra1195@gmail.com>
2021-10-05 20:52:45 -0400

    Add project files.

---
+++ QA_Assignment2/Rectangle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QA_Assignment2
+{
+    public class Rectangle
+    {
+        private int width;
+        private int height;
+
+        public Rectangle()
+        {
+            this.width = 1;
+            this.height = 1;
+        }
+
+        public Rectangle(int width, int height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public int Height { get => height; set => height = value; }
+        public int Width { get => width; set => width = value; }
+    }
+}
